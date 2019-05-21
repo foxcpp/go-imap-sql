add0ce9efde8c4c0f3e334c71fee4679b8dd1e11
fox.cpp <fox.cpp@disroot.org>
2019-05-21 15:01:26 +0300

    Remove Version* constants
    Turned out to be useless.

--- version.go
+++ version.go
@@ -5,10 +5,6 @@ package imapsql
 // Meant for debug logs, you may want to know which go-imap-sql version users
 // have. See below for per-component variants.
 const VersionStr = "0.2"
-const VersionMajor = 0
-const VersionMinor = 1
-const VersionPatch = 0
-const VersionSuppl = ""
 
 // SchemaVersion is incremented each time DB schema changes.
 const SchemaVersion = 2

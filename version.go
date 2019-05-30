d2b8cccce46bcacd3a17571ed9c6e82a1502c4d5
fox.cpp <fox.cpp@disroot.org>
2019-05-30 17:34:36 +0300

    Update VersionStr constant

--- version.go
+++ version.go
@@ -3,8 +3,8 @@ package imapsql
 // VersionStr is a string value representing go-imap-sql version.
 //
 // Meant for debug logs, you may want to know which go-imap-sql version users
-// have. See below for per-component variants.
-const VersionStr = "0.2"
+// have.
+const VersionStr = "0.3.1"
 
 // SchemaVersion is incremented each time DB schema changes.
 const SchemaVersion = 3

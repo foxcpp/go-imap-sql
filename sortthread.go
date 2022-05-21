fd47f96410d5f91f93f845fe321fe541c9aa67d8
ptrcnull <git@ptrcnull.me>
2022-05-21 03:25:26 +0200

    Parse date correctly while sorting messages

--- sortthread.go
+++ sortthread.go
@@ -77,7 +77,7 @@ func firstAddrFromList(all []string) string {
 }
 
 func sentDate(dateHeaders []string, arrivalUnix int64) time.Time {
-	t, err := mail.ParseDate(firstHeaderField(dateHeaders))
+	t, err := parseMessageDateTime(firstHeaderField(dateHeaders))
 	if err != nil {
 		return time.Unix(arrivalUnix, 0)
 	}
